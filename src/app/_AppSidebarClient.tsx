ff0d2fe851e6c0acb87c651959e92f6cbffe3845
Karan Kadyan <karankadyan@Karans-MacBook-Air.local>
2025-07-29 14:20:16 +0530

    “sidebar”

--- src/app/_AppSidebarClient.tsx
+++ src/app/_AppSidebarClient.tsx
@@ -2,14 +2,23 @@
 
 import React from "react";
 import {useIsMobile} from "@/hooks/use-mobile";
+import {SidebarTrigger} from "@/components/ui/sidebar";
 
-export function AppSidebarClient({ children } : {
-  children: React.ReactNode
+export function AppSidebarClient({children}: {
+    children: React.ReactNode
 }) {
     const isMobile = useIsMobile()
 
     if (isMobile) {
-        return null
+        return (
+            <div className="flex flex-col w-full">
+                <div className="p-2 border-b flex items-center gap-1">
+                    <SidebarTrigger />
+                    <span className="mt-0.5 text-xl tracking-wide font-light">Hirevo</span>
+                </div>
+                <div className="flex-1 flex">{children}</div>
+            </div>
+        )
     }
     return children;
 }
