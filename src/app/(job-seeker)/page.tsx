e2672091c8c99f03593c7805c4e58bf48f5730de
Karan Kadyan <karankadyan@Karans-MacBook-Air.local>
2025-08-04 14:22:33 +0530

    job-seeker main page

--- src/app/(job-seeker)/page.tsx
+++ src/app/(job-seeker)/page.tsx
@@ -1,7 +1,13 @@
-export default function HomePage() {
+import { JobListingItems } from "./_shared/JobListingItems"
+
+export default function HomePage({
+                                     searchParams,
+                                 }: {
+    searchParams: Promise<Record<string, string | string[]>>
+}) {
     return (
         <div className="m-4">
-
+            <JobListingItems searchParams={searchParams} />
         </div>
     )
 }
